ca530c1fb0599290720d5de1a26eb474297b499e
NGageMegaman <gerardgery1@gmail.com>
2019-12-14 16:17:59 +0100

    music&levelTitles

---
+++ Assets/Scripts/TextLevelName.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextLevelName : MonoBehaviour
+{
+    public RectTransform rt;
+    public int level;
+    public Text t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rt = gameObject.GetComponent<RectTransform>();
+
+        t = gameObject.GetComponent<Text>();
+
+        if (level == 1)
+        {
+            t.text = "HELL ESCAPE";
+        }
+        else if (level == 2)
+        {
+            t.text = "MOUNTAIN TRAIL";
+        }
+        else if (level == 3)
+        {
+            t.text = "HEAVEN'S DOOR";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = rt.localPosition;
+        if (pos.x > 50) rt.localPosition = new Vector3(pos.x - 10, pos.y, pos.z);
+        else if (pos.x > -50) rt.localPosition = new Vector3(pos.x - 2, pos.y, pos.z);
+        else if (pos.x > -1000) rt.localPosition = new Vector3(pos.x - 10, pos.y, pos.z);
+        else gameObject.SetActive(false);
+    }
+}
